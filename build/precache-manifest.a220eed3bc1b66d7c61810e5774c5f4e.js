29832dea62b666b6006f0c7913e64f7ed4283ac5
ucims <ucims30@gmail.com>
2020-07-12 06:36:32 +0700

    deploy firebase

---
+++ build/precache-manifest.a220eed3bc1b66d7c61810e5774c5f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754892be388b669eee3efc120f121bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e1e62848ac7d909a27",
+    "url": "/static/css/main.7791b326.chunk.css"
+  },
+  {
+    "revision": "6eaa22916003fb21a496",
+    "url": "/static/js/2.05ff066f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05ff066f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e1e62848ac7d909a27",
+    "url": "/static/js/main.15782491.chunk.js"
+  },
+  {
+    "revision": "4080a161469f9c399022",
+    "url": "/static/js/runtime-main.a8e335d7.js"
+  }
+]);
